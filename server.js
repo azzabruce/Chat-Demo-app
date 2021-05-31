b5c1dbbcb960bfbb500f9f12584cba244a362edb
azzabruce <azzabbruce@gmail.com>
2021-05-31 11:14:45 -0400

    connected to mongodb

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var app = express()
 var http = require('http').Server(app)
 //create io and set it require socket io and will pass in reference to http
 var io = require('socket.io')(http)
+//require mongoose and connect to our remote database
+var mongoose = require('mongoose')
 
 app.use(express.static(__dirname))
 //this let bodyParser know that we expect json to be coming in with http request
@@ -13,32 +15,61 @@ app.use(bodyParser.json())
 //at this point when we click send, we recieve an embty object, because the broweser is urlencoded
 //so we must setup a bodyParser to support that
 app.use(bodyParser.urlencoded({ extended: false }))
+
+//creat a dbUrl
+var dbUrl = "mongodb+srv://user:user@cluster0.kj3lc.mongodb.net/test"
+// we want to create a messages Model 
+
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
+})
 //at this point, we have to refresh the page to see new messages
 //we would need to install socket.ip 
 //needs to tie in with expresswe will create a regular http server with node and share with express and node
 //let's create a placeholder messages list as an array
-var messages = [
-    { name: 'Azza', message: 'Hello' },
-    { name: 'Tim', message: 'Hi' }
-]
+//we don't need our message array becuase data is coming from mongodb
+// var messages = [
+//     { name: 'Azza', message: 'Hello' },
+//     { name: 'Tim', message: 'Hi' }
+// ]
 // 1. creating a get messages end service in node, this allows messages to be called from the backend and displayed in the front-end 
 // 2. we need to add a route for endpoint, we will be using app.get to sepcifiy that we will be using a get request
 app.get('/messages', (req, res) => {//the route is '/messages. then a callback to handle the request take in response and give is refernce to res to response to it
-    res.send(messages) // next step is to update the front-end to call the messages from backend
+    //instead of sending our message array, we will send messages from mongoDB
+    //to get all the messages in the database (mongodb), we use {} empty brackets
+    Message.find({}, (err, messages) => {
+        res.send(messages) // next step is to update the front-end to call the messages from backend
+    })
+
 })
 //let's create a new message post endpoint in node
 app.post('/messages', (req, res) => {
-    console.log(req.body)// this will return undefined since express doesn't have a built in support to parse the body, we need to install body-parser
-    messages.push(req.body)//add he new message to our message array from postman
-    //let's emit an event from the server to all clients notifying them, there is a new message
-    io.emit('message', req.body)//message is the event and req.body will contain the message
-    res.sendStatus(200)
+    // create an object based on Message model and pass req.boy because it contains our message definition
+    var messsage = new Message(req.body)
+    //let's save it to mongoDB
+    messsage.save((err) => {
+        //if there is an error, send a server erro 500
+        if (err)
+            sendStatus(500)
+        // this will return undefined since express doesn't have a built in support to parse the body, we need to install body-parser
+        // console.log(req.body)
+        // messages.push(req.body)//add he new message to our message array from postman
+        //let's emit an event from the server to all clients notifying them, there is a new message
+        io.emit('message', req.body)//message is the event and req.body will contain the message
+        res.sendStatus(200)
+    })
+
 
 })
 //let's set up a callback for the scoket coneection event, that will let us know when a new user connect
 //check for coneection events and will supply a function that takes in a scoket 
 io.on('connection', (socket) => {
     console.log('a user connected')
+})
+//connect to mnogoose
+mongoose.connect(dbUrl, { seNewUrlParser: true }, (err) => {
+    console.log('mongodb connect is successfull', err)
 })
 var server = http.listen(3000, () => {
     console.log('server is listening on port', server.address().port)
